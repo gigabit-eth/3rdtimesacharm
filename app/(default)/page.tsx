b1a8d23aed582a9525750c7b66c86c5e171cb656
gigabit-eth <ogpuzzle9@gmail.com>
2024-01-31 02:56:30 -0800

    gtm bugs

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,24 +1,11 @@
 "use client";
 import React, { useEffect } from "react";
-import TagManager from "react-gtm-module";
-
 import Hero from "@/components/hero";
 import Features from "@/components/features";
 import FeaturesBlocks from "@/components/features-blocks";
 import Testimonials from "@/components/testimonials";
 import Newsletter from "@/components/newsletter";
 import HowToBuy from "@/components/buy";
-
-const tagManagerArgs = {
-  gtmId: "GTM-KLB2GV6V",
-};
-
-TagManager.initialize(tagManagerArgs);
-
-const metadata = {
-  title: "$GMEStop on Solana",
-  description: "They Can't Halt This One.",
-};
 
 export default function Home() {
   useEffect(() => {
