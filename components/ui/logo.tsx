7b7a5ba9d653e8fc9ee858ee48b0786a8200daf1
gigabit-eth <ogpuzzle9@gmail.com>
2024-01-31 17:41:28 -0800

    new brand logo & favicon

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
 import WSB from "@/components/ui/wsb";
+import WordMark from "./wordmark";
 
 export default function Logo() {
   return (
     <Link href="/" className="block" aria-label="$GMEonSol">
-      <WSB />
+      {/* <WSB /> */}
+      <WordMark />
     </Link>
   );
 }
