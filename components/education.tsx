8e761ceb25359175f74721278bc075ea2efea6d8
gigabit-eth <ogpuzzle9@gmail.com>
2024-02-18 14:06:13 -0800

    larger card surface area click

--- components/education.tsx
+++ components/education.tsx
@@ -25,240 +25,242 @@ export default function Education() {
           {/* Items */}
           <div className="max-w-sm mx-auto grid gap-6 md:grid-cols-2 lg:grid-cols-3 items-start md:max-w-2xl lg:max-w-none">
             {/* 1st item */}
-            <div className="relative flex flex-col items-center p-6 bg-zinc-800 rounded shadow-xl">
-              <svg
-                className="w-16 h-16 p-1 -mt-1 mb-2"
-                viewBox="0 0 64 64"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <g fill="none" fillRule="evenodd">
-                  <rect
-                    className="fill-current text-zinc-900"
-                    width="64"
-                    height="64"
-                    rx="32"
-                  />
-                  <g strokeWidth="2">
-                    <path
-                      className="stroke-current text-blue-300"
-                      d="M34.514 35.429l2.057 2.285h8M20.571 26.286h5.715l2.057 2.285"
+            <a
+              href="https://www.youtube.com/watch?v=V7pcZ3-vYeM"
+              target="_blank"
+            >
+              <div className="relative flex flex-col items-center p-6 bg-zinc-800 rounded shadow-xl">
+                <svg
+                  className="w-16 h-16 p-1 -mt-1 mb-2"
+                  viewBox="0 0 64 64"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g fill="none" fillRule="evenodd">
+                    <rect
+                      className="fill-current text-zinc-900"
+                      width="64"
+                      height="64"
+                      rx="32"
                     />
-                    <path
-                      className="stroke-current text-white"
-                      d="M20.571 37.714h5.715L36.57 26.286h8"
-                    />
-                    <path
-                      className="stroke-current text-blue-300"
-                      strokeLinecap="square"
-                      d="M41.143 34.286l3.428 3.428-3.428 3.429"
-                    />
-                    <path
-                      className="stroke-current text-white"
-                      strokeLinecap="square"
-                      d="M41.143 29.714l3.428-3.428-3.428-3.429"
-                    />
+                    <g strokeWidth="2">
+                      <path
+                        className="stroke-current text-blue-300"
+                        d="M34.514 35.429l2.057 2.285h8M20.571 26.286h5.715l2.057 2.285"
+                      />
+                      <path
+                        className="stroke-current text-white"
+                        d="M20.571 37.714h5.715L36.57 26.286h8"
+                      />
+                      <path
+                        className="stroke-current text-blue-300"
+                        strokeLinecap="square"
+                        d="M41.143 34.286l3.428 3.428-3.428 3.429"
+                      />
+                      <path
+                        className="stroke-current text-white"
+                        strokeLinecap="square"
+                        d="M41.143 29.714l3.428-3.428-3.428-3.429"
+                      />
+                    </g>
                   </g>
-                </g>
-              </svg>
-              <a
-                href="https://www.youtube.com/watch?v=V7pcZ3-vYeM"
-                target="_blank"
-              >
+                </svg>
                 <h4 className="text-xl font-bold leading-snug tracking-tight mb-1 text-gray-200">
                   How to Buy $GME
                 </h4>
-              </a>
-              <p className="text-gray-600 text-center">
-                Easy How-To guide from an OG in the space!
-              </p>
-            </div>
+                <p className="text-gray-600 text-center">
+                  Easy How-To guide from an OG in the space!
+                </p>
+              </div>
+            </a>
 
             {/* 2nd item */}
-            <div className="relative flex flex-col items-center p-6 bg-zinc-800 rounded shadow-xl">
-              <svg
-                className="w-16 h-16 p-1 -mt-1 mb-2"
-                viewBox="0 0 64 64"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <g fill="none" fillRule="evenodd">
-                  <rect
-                    className="fill-current text-zinc-900"
-                    width="64"
-                    height="64"
-                    rx="32"
-                  />
-                  <g strokeWidth="2" transform="translate(19.429 20.571)">
-                    <circle
-                      className="stroke-current text-white"
-                      strokeLinecap="square"
-                      cx="12.571"
-                      cy="12.571"
-                      r="1.143"
+            <a
+              href="https://www.youtube.com/watch?v=GAS-AfBDlIw"
+              target="_blank"
+            >
+              <div className="relative flex flex-col items-center p-6 bg-zinc-800 rounded shadow-xl">
+                <svg
+                  className="w-16 h-16 p-1 -mt-1 mb-2"
+                  viewBox="0 0 64 64"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g fill="none" fillRule="evenodd">
+                    <rect
+                      className="fill-current text-zinc-900"
+                      width="64"
+                      height="64"
+                      rx="32"
                     />
-                    <path
-                      className="stroke-current text-white"
-                      d="M19.153 23.267c3.59-2.213 5.99-6.169 5.99-10.696C25.143 5.63 19.514 0 12.57 0 5.63 0 0 5.629 0 12.571c0 4.527 2.4 8.483 5.99 10.696"
-                    />
-                    <path
-                      className="stroke-current text-blue-300"
-                      d="M16.161 18.406a6.848 6.848 0 003.268-5.835 6.857 6.857 0 00-6.858-6.857 6.857 6.857 0 00-6.857 6.857 6.848 6.848 0 003.268 5.835"
-                    />
+                    <g strokeWidth="2" transform="translate(19.429 20.571)">
+                      <circle
+                        className="stroke-current text-white"
+                        strokeLinecap="square"
+                        cx="12.571"
+                        cy="12.571"
+                        r="1.143"
+                      />
+                      <path
+                        className="stroke-current text-white"
+                        d="M19.153 23.267c3.59-2.213 5.99-6.169 5.99-10.696C25.143 5.63 19.514 0 12.57 0 5.63 0 0 5.629 0 12.571c0 4.527 2.4 8.483 5.99 10.696"
+                      />
+                      <path
+                        className="stroke-current text-blue-300"
+                        d="M16.161 18.406a6.848 6.848 0 003.268-5.835 6.857 6.857 0 00-6.858-6.857 6.857 6.857 0 00-6.857 6.857 6.848 6.848 0 003.268 5.835"
+                      />
+                    </g>
                   </g>
-                </g>
-              </svg>
-              <a
-                href="https://www.youtube.com/watch?v=GAS-AfBDlIw"
-                target="_blank"
-              >
+                </svg>
                 <h4 className="text-lg font-bold leading-snug tracking-tight mb-1 text-gray-200">
                   Buy $GME w/ Phantom Wallet
                 </h4>
-              </a>
-              <p className="text-gray-600 text-center">
-                Another great community member How-To buy video!
-              </p>
-            </div>
+                <p className="text-gray-600 text-center">
+                  Another great community member How-To buy video!
+                </p>
+              </div>
+            </a>
 
             {/* 3rd item */}
-            <div className="relative flex flex-col items-center p-6 bg-zinc-800 rounded shadow-xl">
-              <svg
-                className="w-16 h-16 p-1 -mt-1 mb-2"
-                viewBox="0 0 64 64"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <g fill="none" fillRule="evenodd">
-                  <rect
-                    className="fill-current text-zinc-900"
-                    width="64"
-                    height="64"
-                    rx="32"
-                  />
-                  <g strokeWidth="2">
-                    <path
-                      className="stroke-current text-blue-300"
-                      d="M34.743 29.714L36.57 32 27.43 43.429H24M24 20.571h3.429l1.828 2.286"
+            <a
+              href="https://phantom.app/learn/blog/security-at-phantom"
+              target="_blank"
+            >
+              <div className="relative flex flex-col items-center p-6 bg-zinc-800 rounded shadow-xl">
+                <svg
+                  className="w-16 h-16 p-1 -mt-1 mb-2"
+                  viewBox="0 0 64 64"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g fill="none" fillRule="evenodd">
+                    <rect
+                      className="fill-current text-zinc-900"
+                      width="64"
+                      height="64"
+                      rx="32"
                     />
-                    <path
-                      className="stroke-current text-white"
-                      strokeLinecap="square"
-                      d="M34.743 41.143l1.828 2.286H40M40 20.571h-3.429L27.43 32l1.828 2.286"
-                    />
-                    <path
-                      className="stroke-current text-blue-300"
-                      d="M36.571 32H40"
-                    />
-                    <path
-                      className="stroke-current text-white"
-                      d="M24 32h3.429"
-                      strokeLinecap="square"
-                    />
+                    <g strokeWidth="2">
+                      <path
+                        className="stroke-current text-blue-300"
+                        d="M34.743 29.714L36.57 32 27.43 43.429H24M24 20.571h3.429l1.828 2.286"
+                      />
+                      <path
+                        className="stroke-current text-white"
+                        strokeLinecap="square"
+                        d="M34.743 41.143l1.828 2.286H40M40 20.571h-3.429L27.43 32l1.828 2.286"
+                      />
+                      <path
+                        className="stroke-current text-blue-300"
+                        d="M36.571 32H40"
+                      />
+                      <path
+                        className="stroke-current text-white"
+                        d="M24 32h3.429"
+                        strokeLinecap="square"
+                      />
+                    </g>
                   </g>
-                </g>
-              </svg>
-              <a
-                href="https://phantom.app/learn/blog/security-at-phantom"
-                target="_blank"
-              >
+                </svg>
+
                 <h4 className="text-xl font-bold leading-snug tracking-tight mb-1 text-gray-200">
                   Phantom Wallet Security
                 </h4>
-              </a>
-              <p className="text-gray-600 text-center text-sm">
-                Basic wallet security, scammer/hacker attacks, handle spam coins
-                & nfts
-              </p>
-            </div>
+                <p className="text-gray-600 text-center text-sm">
+                  Basic wallet security, scammer/hacker attacks, handle spam
+                  coins & nfts
+                </p>
+              </div>
+            </a>
 
             {/* 4th item */}
-            <div className="relative flex flex-col items-center p-6 bg-zinc-800 rounded shadow-xl">
-              <svg
-                className="w-16 h-16 p-1 -mt-1 mb-2"
-                viewBox="0 0 64 64"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <g fill="none" fillRule="evenodd">
-                  <rect
-                    className="fill-current text-zinc-900"
-                    width="64"
-                    height="64"
-                    rx="32"
-                  />
-                  <g strokeWidth="2">
-                    <path
-                      className="stroke-current text-white"
-                      d="M32 37.714A5.714 5.714 0 0037.714 32a5.714 5.714 0 005.715 5.714"
+            <a
+              href="https://nordiccrypto.io/tangem-wallet-vs-ledger-which-wallet-is-the-best/"
+              target="_blank"
+            >
+              <div className="relative flex flex-col items-center p-6 bg-zinc-800 rounded shadow-xl">
+                <svg
+                  className="w-16 h-16 p-1 -mt-1 mb-2"
+                  viewBox="0 0 64 64"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g fill="none" fillRule="evenodd">
+                    <rect
+                      className="fill-current text-zinc-900"
+                      width="64"
+                      height="64"
+                      rx="32"
                     />
-                    <path
-                      className="stroke-current text-white"
-                      d="M32 37.714a5.714 5.714 0 015.714 5.715 5.714 5.714 0 015.715-5.715M20.571 26.286a5.714 5.714 0 005.715-5.715A5.714 5.714 0 0032 26.286"
-                    />
-                    <path
-                      className="stroke-current text-white"
-                      d="M20.571 26.286A5.714 5.714 0 0126.286 32 5.714 5.714 0 0132 26.286"
-                    />
-                    <path
-                      className="stroke-current text-blue-300"
-                      d="M21.714 40h4.572M24 37.714v4.572M37.714 24h4.572M40 21.714v4.572"
-                      strokeLinecap="square"
-                    />
+                    <g strokeWidth="2">
+                      <path
+                        className="stroke-current text-white"
+                        d="M32 37.714A5.714 5.714 0 0037.714 32a5.714 5.714 0 005.715 5.714"
+                      />
+                      <path
+                        className="stroke-current text-white"
+                        d="M32 37.714a5.714 5.714 0 015.714 5.715 5.714 5.714 0 015.715-5.715M20.571 26.286a5.714 5.714 0 005.715-5.715A5.714 5.714 0 0032 26.286"
+                      />
+                      <path
+                        className="stroke-current text-white"
+                        d="M20.571 26.286A5.714 5.714 0 0126.286 32 5.714 5.714 0 0132 26.286"
+                      />
+                      <path
+                        className="stroke-current text-blue-300"
+                        d="M21.714 40h4.572M24 37.714v4.572M37.714 24h4.572M40 21.714v4.572"
+                        strokeLinecap="square"
+                      />
+                    </g>
                   </g>
-                </g>
-              </svg>
-              <a
-                href="https://nordiccrypto.io/tangem-wallet-vs-ledger-which-wallet-is-the-best/"
-                target="_blank"
-              >
+                </svg>
+
                 <h4 className="text-xl font-bold leading-snug tracking-tight mb-1 text-gray-200">
                   Hardware Cold Storage
                 </h4>
-              </a>
-              <p className="text-gray-600 text-center">
-                Highly recommended way to store your digital assets
-              </p>
-            </div>
+                <p className="text-gray-600 text-center">
+                  Highly recommended way to store your digital assets
+                </p>
+              </div>
+            </a>
 
             {/* 5th item */}
-            <div className="relative flex flex-col items-center p-6 bg-zinc-800 rounded shadow-xl">
-              <svg
-                className="w-16 h-16 p-1 -mt-1 mb-2"
-                viewBox="0 0 64 64"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <g fill="none" fillRule="evenodd">
-                  <rect
-                    className="fill-current text-zinc-900"
-                    width="64"
-                    height="64"
-                    rx="32"
-                  />
-                  <g strokeWidth="2">
-                    <path
-                      className="stroke-current text-white"
-                      d="M19.429 32a12.571 12.571 0 0021.46 8.89L23.111 23.11A12.528 12.528 0 0019.429 32z"
+            <a
+              href="https://medium.com/coinmonks/you-need-a-sol-domain-right-now-e2c2d2699501"
+              target="_blank"
+            >
+              <div className="relative flex flex-col items-center p-6 bg-zinc-800 rounded shadow-xl">
+                <svg
+                  className="w-16 h-16 p-1 -mt-1 mb-2"
+                  viewBox="0 0 64 64"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g fill="none" fillRule="evenodd">
+                    <rect
+                      className="fill-current text-zinc-900"
+                      width="64"
+                      height="64"
+                      rx="32"
                     />
-                    <path
-                      className="stroke-current text-blue-300"
-                      d="M32 19.429c6.943 0 12.571 5.628 12.571 12.571M32 24a8 8 0 018 8"
-                    />
-                    <path
-                      className="stroke-current text-white"
-                      d="M34.286 29.714L32 32"
-                    />
+                    <g strokeWidth="2">
+                      <path
+                        className="stroke-current text-white"
+                        d="M19.429 32a12.571 12.571 0 0021.46 8.89L23.111 23.11A12.528 12.528 0 0019.429 32z"
+                      />
+                      <path
+                        className="stroke-current text-blue-300"
+                        d="M32 19.429c6.943 0 12.571 5.628 12.571 12.571M32 24a8 8 0 018 8"
+                      />
+                      <path
+                        className="stroke-current text-white"
+                        d="M34.286 29.714L32 32"
+                      />
+                    </g>
                   </g>
-                </g>
-              </svg>
-              <a
-                href="https://medium.com/coinmonks/you-need-a-sol-domain-right-now-e2c2d2699501"
-                target="_blank"
-              >
+                </svg>
                 <h4 className="text-xl font-bold leading-snug tracking-tight mb-1 text-gray-200">
                   Why a .SOL Domain?
                 </h4>
-              </a>
-              <p className="text-gray-600 text-center">
-                Simple explanation of the benefits of owning a .SOL domain
-              </p>
-            </div>
+                <p className="text-gray-600 text-center">
+                  Simple explanation of the benefits of owning a .SOL domain
+                </p>
+              </div>
+            </a>
 
             {/* 6th item */}
             <div className="relative flex flex-col items-center p-6 bg-zinc-800 rounded shadow-xl">
